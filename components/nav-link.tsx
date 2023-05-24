98266433543aaaeb27fd5b305dbd03ffb243d409
Victor Avila <vavilamo@cisco.com>
2023-05-24 15:58:13 -0700

    feat: update toolbar

--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -15,11 +15,16 @@ export const NavLink = ({ children, buttonProps, ...props }: NavLinkProps) => {
   const active = props.href === `/${segment}`;
 
   return (
-    <Button asChild variant="link" size="sm" {...buttonProps}>
+    <Button
+      asChild={!buttonProps?.disabled}
+      variant="link"
+      size="sm"
+      {...buttonProps}
+    >
       <Link
         className={cn(
           "relative h-7 text-base",
-          active && "text-red-900 font-bold",
+          active && "text-cyan-900 font-bold",
           "hover:no-underline hover:after:scale-x-100",
           "relative inline-flex text-sm before:absolute before:left-0 before:bottom-0 before:h-[0.05rem] before:w-full before:origin-right before:scale-x-0 before:bg-cyan-500 before:transition-transform before:duration-300 hover:before:origin-left hover:before:scale-x-100"
         )}
